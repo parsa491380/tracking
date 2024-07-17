a03e91e7dbaceb2d37ab1398a43ce6bcee4cdf58
o.akbari <o.akbari@ofogh.com>
2024-07-17 17:34:52 +0330

    request sucsessfully created and result apeared

---
+++ src/Types/adressResponse.ts
@@ -0,0 +1,14 @@
+type adressResponse = {
+ boundingbox: string[];
+ class: string;
+ display_name: string;
+ importance: number;
+ lat: string;
+ licence: string;
+ lon: string;
+ osm_id: number;
+ osm_type: string;
+ place_id: number;
+ type: string;
+};
+export default adressResponse;
--- src/components/Search&Menu/AdressTextField.tsx
+++ src/components/Search&Menu/AdressTextField.tsx
@@ -7,12 +7,21 @@ import "../../Assets/Styles/AdressTextField.css";
 import L from "leaflet";
 import { useMap } from "react-leaflet";
 import axios from "axios";
-import { Box, InputBase } from "@mui/material";
+import {
+ Box,
+ InputBase,
+ List,
+ ListItem,
+ ListItemButton,
+ ListItemText,
+ Typography,
+} from "@mui/material";
+import type adressResponse from "../../Types/adressResponse.ts";
 
 export default function AdressTextField() {
  const [position, setPosition] = useState(null);
  const [adress, setAdress] = useState<string>("");
- const [data, setData] = useState();
+ const [data, setData] = useState<adressResponse[] | null>(null);
 
  const map = useMap();
 
@@ -26,7 +35,6 @@ export default function AdressTextField() {
   map.locate();
   map.once("locationfound", (location) => {
    setPosition(location.latlng);
-   console.log(location.latlng);
   });
 
   // my personal API KEY  is : 66978130a068a775898956bsw7efe7a
@@ -44,42 +52,72 @@ export default function AdressTextField() {
 
  const handleEnter = (e) => {
   if (e.key === "Enter") {
-   console.log("do validate");
+   routingDirectly();
   }
  };
 
- return (
-  <Paper id="paperStyle" ref={divRef}>
-   <SideMenu />
-   <Box
-    onSubmit={(event) => {
-     console.log("aaaa");
-     event.preventDefault();
+ const RenderOptions = (item) => {
+  const select = () => {
+   setAdress(item.item.item.display_name);
+  };
+  return (
+   <Typography onClick={select}> {item.item.item.display_name}</Typography>
+  );
+  //   return (
+  //    <ListItem
+  //     key={item.item.index}
+  //     onClick={select}
+  //     component="div"
+  //     disablePadding
+  //     sx={{ fontSize: "small" }}
+  //    >
+  //     <ListItemButton>
+  //      <ListItemText primary={` ${item.item.item.display_name}`} />
+  //     </ListItemButton>
+  //    </ListItem>
+  //   );
+ };
 
-     routingDirectly();
-    }}
-   >
-    <InputBase
-     id="input"
-     placeholder="routing directly"
-     type="search"
-     value={adress}
-     onKeyDown={handleEnter}
-     inputProps={{ "aria-label": "search google maps" }}
-     onChange={(e) => {
-      setAdress(e.target.value);
-     }}
-    />
-    <IconButton
-     type="button"
-     sx={{ p: "10px", color: "#0ab6ff" }}
-     aria-label="search"
-     onClick={routingDirectly}
+ return (
+  <>
+   <Paper id="paperStyle" ref={divRef}>
+    <SideMenu />
+    <Box
+    // onSubmit={(event) => {
+    //  console.log("aaaa");
+    //  event.preventDefault();
+
+    //  routingDirectly();
+    // }}
     >
-     <SearchIcon />
-    </IconButton>
-   </Box>
-   {}
-  </Paper>
+     <InputBase
+      id="input"
+      placeholder="routing directly"
+      type="search"
+      value={adress}
+      onKeyDown={handleEnter}
+      inputProps={{ "aria-label": "search google maps" }}
+      onChange={(e) => {
+       setAdress(e.target.value);
+      }}
+     />
+     <IconButton
+      type="button"
+      sx={{ p: "10px", color: "#0ab6ff" }}
+      aria-label="search"
+      onClick={routingDirectly}
+     >
+      <SearchIcon />
+     </IconButton>
+    </Box>
+   </Paper>
+   <Paper sx={{ width: 500 }}>
+    {data !== null
+     ? data.map((item, index) => {
+        return <RenderOptions item={{ item, index }} key={index} />;
+       })
+     : null}
+   </Paper>
+  </>
  );
 }
